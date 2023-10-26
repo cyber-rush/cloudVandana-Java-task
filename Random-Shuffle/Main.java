d052606df74eaa0f0731c40b413f49204ae43530
Ujjawal Joshi <ujjawaljoshi07@gmail.com>
2023-10-26 20:19:40 +0530

    added the java code for random-shuffle and roman number

---
+++ Random-Shuffle/Main.java
@@ -0,0 +1,24 @@
+import java.util.Random;
+
+public class Main {
+    public static void main(String[] args) {
+        int[] array = {1, 2, 3, 4, 5, 6, 7};
+        shuffleArray(array);
+        
+        // Print shuffled array
+        for (int num : array) {
+            System.out.print(num + " ");
+        }
+    }
+    
+    public static void shuffleArray(int[] array) {
+        Random rand = new Random();
+        for (int i = array.length - 1; i > 0; i--) {
+            int j = rand.nextInt(i + 1);
+            // Swap array[i] and array[j]
+            int temp = array[i];
+            array[i] = array[j];
+            array[j] = temp;
+        }
+    }
+}
---
+++ Roman-Number/Main.java
@@ -0,0 +1,40 @@
+public class Main {
+    public static void main(String[] args) {
+        String romanNumeral = "IX";
+        int result = romanToInt(romanNumeral);
+        System.out.println("Integer representation: " + result);
+    }
+
+    public static int romanToInt(String s) {
+        int result = 0;
+        int prevValue = 0;
+
+        for (int i = s.length() - 1; i >= 0; i--) {
+            char ch = s.charAt(i);
+            int value = romanCharToInt(ch);
+            
+            if (value < prevValue) {
+                result -= value;
+            } else {
+                result += value;
+            }
+
+            prevValue = value;
+        }
+
+        return result;
+    }
+
+    public static int romanCharToInt(char ch) {
+        switch (ch) {
+            case 'I': return 1;
+            case 'V': return 5;
+            case 'X': return 10;
+            case 'L': return 50;
+            case 'C': return 100;
+            case 'D': return 500;
+            case 'M': return 1000;
+            default: return 0;
+        }
+    }
+}
